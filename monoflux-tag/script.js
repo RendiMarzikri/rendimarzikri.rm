46f05e98408f52ceec8cec1a81323162542d75cc
Rendi Marzikri <arternosrendi@gmail.com>
2026-07-26 20:57:43 +0700

    Update script Tag and index.html

--- monoflux-tag/script.js
+++ monoflux-tag/script.js
@@ -1,5 +1,5 @@
 Tag.create("teks2", {
   render(html) {
-    return `<div style="margin: 0; font-size: 27px; font-weight: bold;">${html}</div>`;
+    return `<div style="margin: 0; margin-top: 0; font-size: 29px; font-weight: bold;">${html}</div>`;
   }
 });
